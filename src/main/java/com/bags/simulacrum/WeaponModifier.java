1b73cd7ece4584abb5e90118ecf11a7e7ef0581e
BagsGH <mmills.1153@gmail.com>
2018-08-19 20:11:35 -0400

    A refactor and giant flowerbox comment because this is not intuitive logic at all.

--- src/main/java/com/bags/simulacrum/WeaponModifier.java
+++ src/main/java/com/bags/simulacrum/WeaponModifier.java
@@ -10,8 +10,7 @@ public class WeaponModifier {
     private List<Mod> weaponMods;
     private Weapon originalWeapon;
 
-    public Weapon modifyWeapon(Weapon weapon)
-    {
+    public Weapon modifyWeapon(Weapon weapon) {
         originalWeapon = weapon;
         weaponMods = originalWeapon.getMods();
         Weapon modifiedWeapon = copyWeaponToMod();
@@ -35,7 +34,7 @@ public class WeaponModifier {
     }
 
     private double calculateModdedFireRate() {
-        double fireRateIncrease =  weaponMods.stream().filter(mod -> mod.getFireRateIncrease() != 0).mapToDouble(Mod::getFireRateIncrease).sum();
+        double fireRateIncrease = weaponMods.stream().filter(mod -> mod.getFireRateIncrease() != 0).mapToDouble(Mod::getFireRateIncrease).sum();
         return originalWeapon.getFireRate() * (1 + fireRateIncrease);
     }
 
@@ -56,22 +55,23 @@ public class WeaponModifier {
 
     private double calculateModdedReloadTime() {
         double reloadSpeedIncrease = weaponMods.stream().filter(mod -> mod.getReloadTimeIncrease() != 0.0).mapToDouble(Mod::getReloadTimeIncrease).sum();
-        return originalWeapon.getReloadTime() / ( 1 + reloadSpeedIncrease);
+        return originalWeapon.getReloadTime() / (1 + reloadSpeedIncrease);
     }
 
     private double calculateModdedChargeTime() {
-        double chargeTimeIncrease = 0.0;
-
-        for(Mod mod : weaponMods)
-        {
-            if(mod.getFireRateIncrease() != 0)
-            {
-                chargeTimeIncrease += mod.getFireRateIncrease() * bowFireRateBonusMultiplier(mod.getFireRateIncrease());
-            }
-        }
+        double chargeTimeIncrease = weaponMods.stream().filter(mod -> mod.getFireRateIncrease() != 0).mapToDouble(mod -> mod.getFireRateIncrease() * bowFireRateBonusMultiplier(mod.getFireRateIncrease())).sum();
         return originalWeapon.getChargeTime() / (1 + chargeTimeIncrease);
     }
 
+    /**
+     *  If the weapon type is a bow, it gets a 2.0 multiplier to the individual mod's positive fire rate bonus. If it's a negative fire rate bonus mod the multiplier is just 1.0.
+     *  Other charge weapons are not affected and always have a multiplier of 1.0.
+     *
+     * @param getFireRateIncrease
+     *  The individual mod's raw increase or decrease to fire rate.
+     * @return
+     *  A multiplier to use when calculating an individual mod's affect on the fire rate.
+     */
     private double bowFireRateBonusMultiplier(double getFireRateIncrease) {
         return getFireRateIncrease > 0 && originalWeapon.getType().equals(Weapon.WeaponType.BOW) ? 2.0 : 1.0;
     }
@@ -83,11 +83,11 @@ public class WeaponModifier {
 
     private double calculateModdedCriticalChance() {
         double criticalChanceIncrease = weaponMods.stream().filter(mod -> mod.getCriticalChanceIncrease() != 0).mapToDouble(Mod::getCriticalChanceIncrease).sum();
-        return originalWeapon.getCriticalChance() * ( 1 + criticalChanceIncrease);
+        return originalWeapon.getCriticalChance() * (1 + criticalChanceIncrease);
     }
 
     private double calculateModdedStatusChance() {
         double statusChanceIncrease = weaponMods.stream().filter(mod -> mod.getStatusChanceIncrease() != 0).mapToDouble(Mod::getStatusChanceIncrease).sum();
-        return originalWeapon.getStatusChance() * ( 1 + statusChanceIncrease );
+        return originalWeapon.getStatusChance() * (1 + statusChanceIncrease);
     }
 }
