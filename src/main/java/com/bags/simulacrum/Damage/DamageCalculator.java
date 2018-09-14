4dae874293f1e5e71a3709b30ab21666c7d3cdd7
BagsGH <mmills.1153@gmail.com>
2018-09-14 18:24:32 -0400

    No armor reduction with shields in play, also you CAN headshot through shields.

--- src/main/java/com/bags/simulacrum/Damage/DamageCalculator.java
+++ src/main/java/com/bags/simulacrum/Damage/DamageCalculator.java
@@ -24,10 +24,10 @@ public class DamageCalculator {
 
         double shieldMultiplier = damagingShields ? 1 + damageBonusMapper.getBonus(targetShield.getHealthClass(), damageType) : 1.0;
         double healthMultiplier = !damagingShields /*damagingHealth(baseHealth, targetShieldValue, damageType)*/ ? 1 + damageBonusMapper.getBonus(baseHealth.getHealthClass(), damageType) : 1.0; //TODO: replace with !damagingShields?
-        double headCritModifier = calculateHeadshotAndCriticalModifier(critLevel, targetHeadshotMultiplier, isCorpus, weaponCriticalDamageMultiplier, damagingShields);
+        double headCritModifier = calculateHeadshotAndCriticalModifier(critLevel, targetHeadshotMultiplier, isCorpus, weaponCriticalDamageMultiplier);
 
         double baseDamageModifiers = headCritModifier * shieldMultiplier * healthMultiplier;
-        double armorReduction = 1 + ((getArmorAmount(targetArmor) * (1 - getArmorDamageMultiplier(targetArmor, damageType))) / ARMOR_CONSTANT);
+        double armorReduction = !damagingShields ? 1 + ((getArmorAmount(targetArmor) * (1 - getArmorDamageMultiplier(targetArmor, damageType))) / ARMOR_CONSTANT) : 1.0;
         double finalDamageModifier = baseDamageModifiers / armorReduction;
 
         return damage.getDamageValue() * finalDamageModifier;
@@ -41,13 +41,13 @@ public class DamageCalculator {
         return targetArmor != null ? damageBonusMapper.getBonus(targetArmor.getHealthClass(), damageType) : 0.0;
     }
 
-    private double calculateHeadshotAndCriticalModifier(int critLevel, double headshotMultiplier, boolean isCorpusNoHeadshots, double weaponCriticalDamageMultiplier, boolean damagingShields) {
-        if (isHeadshot(headshotMultiplier, isCorpusNoHeadshots, damagingShields) && !isCritical(critLevel)) { //TODO: Check that shields do block headshots
+    private double calculateHeadshotAndCriticalModifier(int critLevel, double headshotMultiplier, boolean isCorpusNoHeadshots, double weaponCriticalDamageMultiplier) {
+        if (isHeadshot(headshotMultiplier, isCorpusNoHeadshots) && !isCritical(critLevel)) { //TODO: Check that shields do block headshots
             return headshotMultiplier;
         }
         if (isCritical(critLevel)) {
             double critModifier = (critLevel * (weaponCriticalDamageMultiplier - 1)) + 1;
-            return isHeadshot(headshotMultiplier, isCorpusNoHeadshots, damagingShields) ? headshotMultiplier * HEADCRIT_MULTIPLIER * critModifier : critModifier;
+            return isHeadshot(headshotMultiplier, isCorpusNoHeadshots) ? headshotMultiplier * HEADCRIT_MULTIPLIER * critModifier : critModifier;
         }
         return 1.0;
     }
@@ -56,8 +56,8 @@ public class DamageCalculator {
         return critLevel != 0;
     }
 
-    private boolean isHeadshot(double headshotMultiplier, boolean isCorpusNoHeadshots, boolean damagingShields) {
-        return headshotMultiplier != 0 && !isCorpusNoHeadshots && !damagingShields;
+    private boolean isHeadshot(double headshotMultiplier, boolean isCorpusNoHeadshots) {
+        return headshotMultiplier != 0 && !isCorpusNoHeadshots;
     }
 
 //    private boolean damagingHealth(Health baseHealth, double targetShieldValue, DamageType damageType) {
