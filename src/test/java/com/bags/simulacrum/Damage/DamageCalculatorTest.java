fceab1c6fff929eaa6993efac7a73ece9a145113
BagsGH <mmills.1153@gmail.com>
2018-09-15 09:20:39 -0400

    Some new tests and removing Correctly from every method name. Kind of redundant...

--- src/test/java/com/bags/simulacrum/Damage/DamageCalculatorTest.java
+++ src/test/java/com/bags/simulacrum/Damage/DamageCalculatorTest.java
@@ -10,69 +10,128 @@ import static org.junit.Assert.assertEquals;
 public class DamageCalculatorTest {
 
     private DamageCalculator subject;
-    private Damage fake50HeatDamage;
+    private Damage fakeDamage;
+    private Health fakeHealth;
 
     @Before
     public void setup() {
         subject = new DamageCalculator();
-        fake50HeatDamage = new Damage(DamageType.HEAT, 50.0);
+        fakeDamage = new Damage(DamageType.HEAT, 50.0);
+        fakeHealth = new Health(HealthClass.MACHINERY, 200.0);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithNoModifiers() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.MACHINERY, 200.0), null, null, 0.0, false, fake50HeatDamage, 0.0, 0);
+    public void itCanCalculateDamageWithNoModifiers() {
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 0.0, false, fakeDamage, 0.0, 0);
 
         assertEquals(50.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithAPositiveBonusAgainstHealthClass() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.INFESTED_FLESH, 200.0), null, null, 0.0, false, fake50HeatDamage, 0.0, 0);
+    public void itCanCalculateDamageWithAPositiveBonusAgainstHealthClass() {
+        fakeHealth.setHealthClass(HealthClass.INFESTED_FLESH);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 0.0, false, fakeDamage, 0.0, 0);
 
         assertEquals(75.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithANegativeBonusAgainstHealthClass() {
-        fake50HeatDamage.setType(DamageType.COLD);
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.INFESTED_FLESH, 200.0), null, null, 0.0, false, fake50HeatDamage, 0.0, 0);
+    public void itCanCalculateDamageWithANegativeBonusAgainstHealthClass() {
+        fakeDamage.setType(DamageType.VOID);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 0.0, false, fakeDamage, 0.0, 0);
 
         assertEquals(25.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithAHeadshot() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.MACHINERY, 200.0), null, null, 2.0, false, fake50HeatDamage, 0.0, 0);
+    public void itCanCalculateDamageWithAHeadshot() {
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 2.0, false, fakeDamage, 0.0, 0);
 
         assertEquals(100.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithAHeadshot_OnCorpus() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.MACHINERY, 200.0), null, null, 2.0, true, fake50HeatDamage, 0.0, 0);
+    public void itCanCalculateDamageWithAPositiveBonusAgainstHealthClassWithAHeadshot() {
+        fakeHealth.setHealthClass(HealthClass.INFESTED_FLESH);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 3.0, false, fakeDamage, 0.0, 0);
+
+        assertEquals(225.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithAPositiveBonusAgainstHealthClassWithAHeadshot_OnCorpus() {
+        fakeHealth.setHealthClass(HealthClass.INFESTED_FLESH);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 3.0, true, fakeDamage, 0.0, 0);
+
+        assertEquals(75.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithAPositiveBonusAgainstHealthClassWithACriticalHeadshot() {
+        fakeHealth.setHealthClass(HealthClass.INFESTED_FLESH);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 3.0, false, fakeDamage, 3.0, 1);
+
+        assertEquals(1350.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithANegativeBonusAgainstHealthClassWithAHeadshot() {
+        fakeDamage.setType(DamageType.VOID);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 3.0, false, fakeDamage, 0.0, 0);
+
+        assertEquals(75.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithANegativeBonusAgainstHealthClassWithAHeadshot_OnCorpus() {
+        fakeDamage.setType(DamageType.VOID);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 3.0, true, fakeDamage, 0.0, 0);
+
+        assertEquals(25.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithANegativeBonusAgainstHealthClassWithACriticalHeadshot() {
+        fakeDamage.setType(DamageType.VOID);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 3.0, false, fakeDamage, 3.0, 1);
+
+        assertEquals(450.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithAHeadshot_OnCorpus() {
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 2.0, true, fakeDamage, 0.0, 0);
 
         assertEquals(50.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithACritical() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.MACHINERY, 200.0), null, null, 0.0, false, fake50HeatDamage, 2.0, 1);
+    public void itCanCalculateDamageWithACritical() {
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 0.0, false, fakeDamage, 2.0, 1);
 
         assertEquals(100.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithACriticalHeadshot() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.MACHINERY, 200.0), null, null, 2.0, false, fake50HeatDamage, 2.0, 1);
+    public void itCanCalculateDamageWithACriticalHeadshot() {
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 2.0, false, fakeDamage, 2.0, 1);
 
         assertEquals(400.0, actualDamage, 0.001);
     }
 
     @Test
-    public void itCanCorrectlyCalculateDamageWithACriticalHeadshot_OnCorpus() {
-        double actualDamage = subject.calculateDamage(new Health(HealthClass.MACHINERY, 200.0), null, null, 2.0, true, fake50HeatDamage, 2.0, 1);
+    public void itCanCalculateDamageWithACriticalHeadshot_OnCorpus() {
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 2.0, true, fakeDamage, 2.0, 1);
 
         assertEquals(100.0, actualDamage, 0.001);
+    }
+
+    @Test
+    public void itCanCalculateDamageWithAPositiveBonusAgainstHealthClassWithACritical() {
+        fakeHealth.setHealthClass(HealthClass.INFESTED_FLESH);
+        double actualDamage = subject.calculateDamage(fakeHealth, null, null, 0.0, false, fakeDamage, 2.0, 1);
+
+        assertEquals(150.0, actualDamage, 0.001);
     }
 
 }
