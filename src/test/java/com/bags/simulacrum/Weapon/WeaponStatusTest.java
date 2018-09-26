f33ca925be243e79d09ed41b4eac359e3405e663
BagsGH <mmills.1153@gmail.com>
2018-09-25 23:40:20 -0400

    Working on tests.

--- src/test/java/com/bags/simulacrum/Weapon/WeaponStatusTest.java
+++ src/test/java/com/bags/simulacrum/Weapon/WeaponStatusTest.java
@@ -3,34 +3,98 @@ package com.bags.simulacrum.Weapon;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class WeaponStatusTest {
 
     private WeaponStatus subject;
     private FiringProperties fakeFiringProperties;
+    private double fakeDeltaT;
+    private double fakeSpoolSlowdownRatio;
+    private double fakeAutoFireRate;
 
     @Before
     public void before() {
         fakeFiringProperties = new FiringProperties();
-        fakeFiringProperties.setFireRate(15);
-        //fakeFiringProperties.setChargeTime(1.15);
-        fakeFiringProperties.setReloadTime(2.50);
-        fakeFiringProperties.setMagazineSize(200);
-        fakeFiringProperties.setSpoolThreshold(5);
-        fakeFiringProperties.setTriggerType(FiringProperties.TriggerType.AUTOSPOOL);
-        fakeFiringProperties.reloadMagazine();
         subject = new WeaponStatus(fakeFiringProperties);
+        fakeDeltaT = 0.01;
+        fakeSpoolSlowdownRatio = 2.0;
+        fakeAutoFireRate = 15.0;
+    }
+
+    @Test
+    public void itDoesNotSlowDownFireRateForAutoWeapons() {
+        subject = new WeaponStatus(setupFiringProperties(FiringProperties.TriggerType.AUTO, fakeAutoFireRate, 2.5, 200, 0.0, 5));
+
+        int autoTime = 0;
+        int firedCount = 0;
+
+        for (int i = 0; i < 9; i++) {
+            FiringStatus status = subject.progressTime(fakeDeltaT);
+
+            System.out.println(status.getClass()); //TODO: is it doing one too many auto / spools?
+
+            if (status instanceof Auto) {
+                autoTime++;
+            }
+            if (status instanceof Fired) {
+                firedCount++;
+            }
+        }
+
+        int expectedAutoTime = (int) Math.ceil((1 / fakeAutoFireRate) / fakeDeltaT);
+
+        assertEquals(expectedAutoTime, autoTime);
+        assertEquals(2, firedCount);
+    }
+
+    @Test
+    public void itSlowsDownFireRateForSpoolingWeapons() {
+        subject = new WeaponStatus(setupFiringProperties(FiringProperties.TriggerType.AUTOSPOOL, fakeAutoFireRate, 2.5, 200, 0.0, 5));
+
+        int spoolTime = 0;
+        int firedCount = 0;
+
+        for (int i = 0; i < 16; i++) {
+            FiringStatus status = subject.progressTime(fakeDeltaT);
+            if (status instanceof Spooling) {
+                spoolTime++;
+            }
+            if (status instanceof Fired) {
+                firedCount++;
+            }
+        }
+
+        int expectedSpoolingTime = (int) Math.ceil((1 / fakeAutoFireRate * fakeSpoolSlowdownRatio) / fakeDeltaT);
+
+        assertEquals(expectedSpoolingTime, spoolTime);
+        assertEquals(2, firedCount);
     }
 
     @Test
     public void test() {
+        subject = new WeaponStatus(setupFiringProperties(FiringProperties.TriggerType.AUTO, fakeAutoFireRate, 2.5, 200, 0.0, 5));
 
         double runningTime = 0.0;
         for (int i = 0; i < 6000; i++) {
             FiringStatus status = subject.progressTime(0.01);
             runningTime += 0.01;
-            System.out.println("Current time: " + runningTime + ". Current status: " + status.getClass());
+            System.out.println(/*"Current time: " + runningTime + ". Current status: " + */ status.getClass());
         }
 
+    }
+
+    private FiringProperties setupFiringProperties(FiringProperties.TriggerType triggerType, double fireRate, double reloadTime, int magazineSize, double chargeTime, int spoolThreshold) {
+        FiringProperties props = new FiringProperties();
+        props.setFireRate(fireRate);
+        props.setChargeTime(chargeTime);
+        props.setReloadTime(reloadTime);
+        props.setMagazineSize(magazineSize);
+        props.setSpoolThreshold(spoolThreshold);
+        props.setTriggerType(triggerType);
+        props.reloadMagazine(); //TODO: get rid of this
+
+        return props;
     }
 
 }
